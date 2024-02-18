67f6a8ac2c64204e7e693fb8f4747a6c97719eae
Lakshmikant <fullstacktrainer4u@gmail.com>
2024-02-18 12:20:49 +0530

    Added ternary check

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 
 function App() {
+  const name = "Lakshmikant";
+  const isVisible = true;
   return (
     <div className="App">
       <header className="App-header">
-        Welcome to ReactJS World.!
+        Welcome to ReactJS World {isVisible ? name : "Guest Visitor"}.!
       </header>
     </div>
   );
