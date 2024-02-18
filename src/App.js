dc1a5702eb1a2af200bcd703b65ab61a56e67d7e
Lakshmikant <fullstacktrainer4u@gmail.com>
2024-02-18 14:32:08 +0530

    Added omdbAPI

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import './App.css';
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
+const API_URL = "https://www.omdbapi.com/?apikey="; // get apikey from omdb
 function App() {
-  const [counter, setCounter] = useState(0);
+  const [movies, setMovies] = useState([]);
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+    console.log(data);
+    setMovies(data.Search)
+  }
+
+  useEffect(() => { searchMovies("spiderman") }, []);
   return (
     <div className="App">
-      <div>Counter App</div>
-      <button onClick={() => setCounter((prevValue) => prevValue - 1)}>-</button>
-      <h5>{counter}</h5>
-      <button onClick={() => setCounter((prevValue) => prevValue + 1)}>+</button>
+      <div>Movie App</div>
+      {movies.map(movie => <div key={movie.imdbID}>{movie.Title}</div>)}
     </div>
   );
 }
