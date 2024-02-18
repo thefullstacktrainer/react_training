52abeda064b74a69f85fdd197a3a6aa791df6136
Lakshmikant <fullstacktrainer4u@gmail.com>
2024-02-18 12:33:14 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -3,11 +3,27 @@ import './App.css';
 function App() {
   const name = "Lakshmikant";
   const isVisible = true;
+
+  // let address; 
+  // let address = 0;
+  // let address = null;
+  // let address = "";
+  let address = "Hubli";
   return (
     <div className="App">
-      <header className="App-header">
+      <header>
         Welcome to ReactJS World {isVisible ? name : "Guest Visitor"}.!
       </header>
+      <div>
+        {4 + 8}
+      </div>
+      <div>
+        {address ? <>
+          {address}
+        </> : <>
+          <span> There is no address</span>
+        </>}
+      </div>
     </div>
   );
 }
